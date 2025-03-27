92e00cd435e5424ba2f811ac2650ac002e43c39b
kulabin <s31099@pjwstk.edu.pl>
2025-03-27 17:14:11 +0100

    Made Main

--- APBD_03/APBD_03/Program.cs
+++ APBD_03/APBD_03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     public static void Main(string[] args)
     {
+        LiquidContainer liquidContainer =  new LiquidContainer(200,1000,500,10000,true);
+        liquidContainer.loadContainer(200);
         
+        GasContainer gasContainer = new GasContainer(200,1000,500,2000,4000);
+        gasContainer.loadContainer(4000);
+        gasContainer.unloadContainer();
+        Console.WriteLine($"gas container afer unloadint: {gasContainer.mass}");
+        
+        RefridgeratedContainer refridgeratedContainer = new RefridgeratedContainer(200,1000,500,8000,"bananas",13.3,14);
+        refridgeratedContainer.loadContainer(2000);
+
+        ContainerShip containerShip = new ContainerShip(20, 10, 200);
+        containerShip.loadContainer(liquidContainer);
+        containerShip.loadContainer(gasContainer);
+        containerShip.loadContainer(refridgeratedContainer);
+        
+        Console.WriteLine(containerShip);
     }
 }
