9dee51cad0d99bc11b1c6ffa70888f0a90963ddb
kulabin <s31099@pjwstk.edu.pl>
2025-03-27 14:26:32 +0100

    made constructor and variables needed for class overloaded loadContainer and used RequiredTempException

--- APBD_03/APBD_03/RefridgeratedContainer.cs
+++ APBD_03/APBD_03/RefridgeratedContainer.cs
@@ -2,13 +2,28 @@
 
 public class RefridgeratedContainer : BaseContainer
 {
-    public override void loadContainer()
+    public string productType { get; set; }
+    public double temperatureRequired { get; set; }
+    public double currentTemp { get; set; }
+
+    public RefridgeratedContainer(ContainerTypes containerType, double height, double containerMass, double depth, double maxContainerMass, string productType, double temperatureRequired, double currentTemp) : base(containerType, height, containerMass, depth, maxContainerMass)
     {
-        
+        this.productType = productType;
+        this.temperatureRequired = temperatureRequired;
+        this.currentTemp = currentTemp;
+        if (currentTemp<temperatureRequired)
+        {
+            throw  new RequiredTempException("Temperature is too low");
+        }
     }
 
-    public override void unloadContainer()
+    public override void loadContainer(double massToLoad)
     {
-        
+        if (currentTemp<temperatureRequired)
+        {
+            throw new RequiredTempException("Temperature is too low for the product type");
+        }
+        base.loadContainer(massToLoad);
     }
+    
 }
